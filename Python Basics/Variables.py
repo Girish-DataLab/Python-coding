816d03839c861843be866303438aa6123ae3a8ed
GIRISH S <girishs76766@gmail.com>
2026-01-06 21:15:23 +0530

    Variables

---
+++ Python Basics/Variables.py
@@ -0,0 +1,13 @@
+# Variable?
+
+A variable is a name used to store data values in a program. The value of a variable can be changed during program execution.
+
+# Example
+
+name = "Girish"
+course="BCA"
+print(name)
+print(course)
+
+# Girish
+  BCA
