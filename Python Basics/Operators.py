ea3d67630d24402ba32fc2e1530bc241346a6f52
GIRISH S <girishs76766@gmail.com>
2026-01-06 21:41:08 +0530

    How to create Operators

---
+++ Python Basics/Operators.py
@@ -0,0 +1,85 @@
+# Operator
+# An operator is a symbol used to perform operations on variables and values.
+
+# 1. Arithmetic Operators
+# Arithmetic operators are used to perform mathematical calculations.
+
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
+
+#13
+#7
+#30
+#3.3333333333333335
+#1
+#3
+#1000
+
+# 2. Relational (Comparison) Operators
+# Relational operators are used to compare two values.
+
+x = 10
+y = 5
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#False
+#True
+#True
+#False
+#True
+#False
+
+# 3. Logical Operators
+# Logical operators are used to combine conditions.
+
+a = 10
+b = 5
+
+print(a > 5 and b < 10)
+print(a > 5 or b > 10)
+print(not(a > 5))
+
+#True
+#True
+#False
+
+# 4. Assignment Operators
+# Assignment operators are used to assign values to variables.
+
+a = 10
+a += 5
+print(a)
+
+a -= 3
+print(a)
+
+a *= 2
+print(a)
+
+a /= 4
+print(a)
+
+#15
+#12
+#24
+#6.0
+
+
+
+
+
+
