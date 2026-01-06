f87ceccc5058295f60b7641783a419d71c9eb549
GIRISH S <girishs76766@gmail.com>
2026-01-06 21:20:08 +0530

    Data Types

---
+++ Python Basics/Data_Types.py
@@ -0,0 +1,53 @@
+# Data Types
+# Data types specify the type of data that a variable can store, such as numbers, text, or logical values.
+
+# Integer (int)
+# An integer data type is used to store whole numbers without decimal points.
+
+/* Integer*/
+
+a = 10
+b = -5
+print(a)
+print(type(a))
+
+ # 5
+<class 'int'>
+
+# Float (float)
+# A float data type is used to store numbers that contain decimal points.
+
+/* Float */
+
+price = 99.75
+print(price)
+print(type(price))
+
+#  99.75
+<class 'float'>
+
+# String (str)
+# A string data type is used to store text or characters enclosed within single or double quotes.
+
+/* String */
+
+language = "Python"
+print(language)
+print(type(language))
+
+#  Python
+<class 'str'>
+
+#Boolean (bool)
+# A boolean data type stores one of two values: True or False.
+/* Boolean */
+
+is_python_easy = True
+print(is_python_easy)
+print(type(is_python_easy))
+
+#  True
+<class 'bool'>
+
+
+
